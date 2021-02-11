74a5e3646b892afa9e4053a871a20a17dced5f19
luis-amaya <luis.amaya-o@mail.escuelaing.edu.co>
2021-02-10 21:30:28 -0500

    Tarifas Test

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -1,14 +1,70 @@
 package edu.eci.cvds.tdd.aerodescuentos;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.Test;
 
-import org.junit.jupiter.api.Test;
-import  edu.eci.cvds.tdd.aerodescuentos.CalculadorDescuentos
-class TarifasTest {
+public class TarifasTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void calculoTarifaSinDescuento() {
+		double valor = CalculadorDescuentos.calculoTarifa(1000000, 5, 35);
+		Assert.assertTrue(1000000==valor);
 	}
-
+	
+	@Test
+	public void calculoTarifaConDiasDeAnticipacion() {
+		double valor = CalculadorDescuentos.calculoTarifa(1000000, 25, 35);
+		Assert.assertTrue(850000==valor);
+	}
+	
+	@Test
+	public void calculoTarifaConDiasDeAnticipacionYMenorDeEdad() {
+		double valor = CalculadorDescuentos.calculoTarifa(1000000, 21, 16);
+		Assert.assertTrue(800000==valor);
+	}
+	
+	@Test
+	public void calculoTarifaConDiasAnticipacionyMayorDeEdad() {
+		double valor = CalculadorDescuentos.calculoTarifa(1000000, 35, 67);
+		Assert.assertTrue(770000==valor);
+	}
+	
+	@Test
+	public void calculoTarifaMenorDeEdad() {
+		double valor = CalculadorDescuentos.calculoTarifa(1000000, 5, 15);
+		Assert.assertTrue(950000==valor);
+	}
+	
+	@Test
+	public void calculoTarifaMayorDeEdad() {
+		double valor = CalculadorDescuentos.calculoTarifa(1000000, 10, 70);
+		Assert.assertTrue(920000==valor);
+	}
+	
+	@Test
+	public void calculoTarifaEdadNegativa() {
+		try {
+			double valor = CalculadorDescuentos.calculoTarifa(1000000, 10, -70);
+		}catch(Exception e) {
+			Assert.assertTrue(true);
+		}
+	}
+	
+	@Test
+	public void calculoTarifaDiasAnticipacionNegativos() {
+		try {
+			double valor = CalculadorDescuentos.calculoTarifa(1000000, -15, 59);
+		}catch(Exception e) {
+			Assert.assertTrue(true);
+		}
+	}
+	
+	@Test
+	public void calculoTarifaDiasAnticipacionTarifaNegativa() {
+		try {
+			double valor = CalculadorDescuentos.calculoTarifa(-1000000, 5, 25);
+		}catch(Exception e) {
+			Assert.assertTrue(true);
+		}
+	}
 }
